a8fb409abc281ea3f40c2581adc600d0735bc096
heberth-uh <heberth.uh@gmail.com>
2025-09-21 17:14:56 -0600

    feat: Create POST /customers and create a customer

--- src/app/api/customers/route.ts
+++ src/app/api/customers/route.ts
@@ -12,3 +12,24 @@ export async function GET(request: NextRequest) {
     return Response.json({ message: "Internal Server Error" }, { status: 500 });
   }
 }
+
+export async function POST(request: NextRequest) {
+  try {
+    const { name, lastname, phone, address } = await request.json();
+
+    const result = await prisma.customer.create({
+      data: {
+        name,
+        lastname,
+        phone,
+        address
+      }
+    });
+    return Response.json({ message: "Customer created successfully", data: result }, { status: 201 });
+  } catch (error) {
+    if (error instanceof Error) {
+      return Response.json({ message: error.message }, { status: 500 });
+    }
+    return Response.json({ message: "Internal Server Error" }, { status: 500 });
+  }
+}
