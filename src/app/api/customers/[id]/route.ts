23b9852d05edb649fbb4560f7b692c47c60358d4
heberth-uh <heberth.uh@gmail.com>
2025-09-21 18:44:03 -0600

    feat: Create GET /customers/id to a single record

--- src/app/api/customers/[id]/route.ts
+++ src/app/api/customers/[id]/route.ts
@@ -1,6 +1,25 @@
 import { prisma } from "@/lib/prisma";
 import { handleApiError } from "@/lib/utils/api-error";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+
+// Get one single customer
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const result = await prisma.customer.findUnique({
+      where: { id: params.id },
+    });
+
+    return NextResponse.json(
+      { message: "Customer retrieved successfully", data: result },
+      { status: 200 }
+    );
+  } catch (error) {
+    return handleApiError(error);
+  }
+}
 
 // Edit a customer
 export async function PUT(
