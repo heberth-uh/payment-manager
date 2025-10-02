f81ea6fa7664f7bda05d83788a17631156e98356
heberth-uh <heberth.uh@gmail.com>
2025-10-01 23:48:18 -0600

    feat: Move sign-out button in the new Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,7 @@
-"use client";
-
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
-import { toast } from "sonner";
-
 export default function Home() {
-  const router = useRouter();
-
-  const handleSignOut = async () => {
-    const { error } = await authClient.signOut({
-      fetchOptions: {
-        onSuccess: () => {
-          router.push("/login");
-          toast.success("Signed out successfully");
-        },
-      },
-    });
-
-    if (error) {
-      toast.error(error.message || "Something went wrong");
-    }
-  };
-
   return (
     <div>
       <h1>Payment Manager App</h1>
-      <button
-        type="button"
-        className="bg-gray-100 p-2 rounded-md border border-gray-300 cursor-pointer"
-        onClick={handleSignOut}
-      >
-        Sign Out
-      </button>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React from "react";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+function Header() {
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    const { error } = await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/login");
+          toast.success("Signed out successfully");
+        },
+      },
+    });
+
+    if (error) {
+      toast.error(error.message || "Something went wrong");
+    }
+  };
+  return (
+    <header className="border border-b-2 py-4">
+      <div className="container mx-auto">
+        <div className="flex items-center justify-between">
+          <h1>Payment Manager</h1>
+          <button
+            type="button"
+            className="bg-gray-100 py-1 px-3 rounded-sm border border-gray-300 cursor-pointer"
+            onClick={handleSignOut}
+          >
+            Sign Out
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
