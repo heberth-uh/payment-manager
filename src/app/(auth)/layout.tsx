104d9ac861810571cb2dca8bbe5e652efdb6c677
heberth-uh <heberth.uh@gmail.com>
2025-10-11 00:50:50 -0600

    feat: Redirect logged-in users away from auth pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,13 @@
+import { getServerSession } from "@/lib/get-session";
+import { redirect } from "next/navigation";
 import React from "react";
 
-export default function AuthLayout({
+export default async function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession();
+  if (session) redirect('/');
   return <main>{children}</main>;
 }
