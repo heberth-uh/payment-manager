9f5af0268ab4a83c8318cc41e1a6ca929155ad85
heberth-uh <heberth.uh@gmail.com>
2025-10-12 21:56:55 -0600

    feat: Add button refresh in customer info

--- src/app/(dashboard)/customers/[id]/page.tsx
+++ src/app/(dashboard)/customers/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useCustomer } from "@/hooks/api/useCustomers";
-import { MapPin, Phone } from "lucide-react";
+import { MapPin, Phone, RefreshCw } from "lucide-react";
 import { useParams } from "next/navigation";
 import React from "react";
 
@@ -10,7 +10,7 @@ function CustomerPage() {
   const customerId = Array.isArray(params.id) ? params.id[0] : params.id;
 
   if (!customerId) return <p>El ID del cliente es inválido</p>;
-  const { customer, loading, error } = useCustomer(customerId);
+  const { customer, loading, error, refetch } = useCustomer(customerId);
 
   if (error) return <p>{error}</p>;
 
@@ -18,9 +18,17 @@ function CustomerPage() {
     <p>Loading</p>
   ) : (
     <div>
-      <h1 className="font-semibold text-lg mb-2">{`${customer?.name}${
-        " " + customer?.lastname
-      }`}</h1>
+      <div className="flex justify-between items-center">
+        <h1 className="font-semibold text-lg mb-2">
+          {`${customer?.name}${" " + customer?.lastname}`}
+        </h1>
+        <button
+          onClick={() => refetch()}
+          className="bg-gray-200 p-2 rounded-md"
+        >
+          <RefreshCw className="size-4" />
+        </button>
+      </div>
 
       <div className="flex items-center gap-2 mb-2">
         <Phone />
